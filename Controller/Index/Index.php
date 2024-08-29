42355f8a93d0126586b59375677dca62a742a858
viraxpress <vira@viraxpress.com>
2024-08-29 15:24:30 +0530

    Adding Newsletter Extension For VX Frontend

---
+++ Controller/Index/Index.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * ViraXpress - https://www.viraxpress.com
+ *
+ * LICENSE AGREEMENT
+ *
+ * This file is part of the ViraXpress package and is licensed under the ViraXpress license agreement.
+ * You can view the full license at:
+ * https://www.viraxpress.com/license
+ *
+ * By utilizing this file, you agree to comply with the terms outlined in the ViraXpress license.
+ *
+ * DISCLAIMER
+ *
+ * Modifications to this file are discouraged to ensure seamless upgrades and compatibility with future releases.
+ *
+ * @category    ViraXpress
+ * @package     ViraXpress_Newsletter
+ * @author      ViraXpress
+ * @copyright   © 2024 ViraXpress (https://www.viraxpress.com/)
+ * @license     https://www.viraxpress.com/license
+ */
+
+declare(strict_types=1);
+
+namespace ViraXpress\Newsletter\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements HttpGetActionInterface
+{
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Execute view action
+     *
+     * @return ResultInterface
+     */
+    public function execute(): ResultInterface
+    {
+        return $this->resultPageFactory->create();
+    }
+}
